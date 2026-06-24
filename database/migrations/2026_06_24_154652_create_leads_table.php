4e91b137600a0e888109c47d7aa8e3aec51363af
Getoari <getoari_g@hotmail.com>
2026-06-24 17:57:58 +0200

    Initialize Laravel Vue application

---
+++ database/migrations/2026_06_24_154652_create_leads_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('last_name');
+
+            $table->string('company')->nullable();
+
+            $table->string('email');
+            $table->string('phone')->nullable();
+
+
+            $table->enum('status', [
+                'new',
+                'contacted',
+                'follow_up',
+                'qualified',
+                'won',
+                'lost'
+            ])->default('new');
+
+
+            $table->integer('score')->default(0);
+
+            $table->enum('temperature',[
+                'cold',
+                'warm',
+                'hot'
+            ])->default('cold');
+
+
+            $table->decimal(
+                'opportunity_value',
+                10,
+                2
+            )->default(0);
+
+
+            $table->timestamp(
+                'last_contacted_at'
+            )->nullable();
+
+
+            $table->text('notes')->nullable();
+
+
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({
+    stats:Object
+})
+
+</script>
+
+
+<template>
+
+<h1>
+CallFlow Dashboard
+</h1>
+
+
+<div>
+
+Hot Leads:
+{{stats.hot}}
+
+</div>
+
+</template>
